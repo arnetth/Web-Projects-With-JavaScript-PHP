d49cc9bb1126f51dd94b9a12db73feb5195d3732
Hannah Arnett <59895006+arnetth@users.noreply.github.com>
2021-09-15 22:21:19 -0400

    added CGT 14100 Final project website

---
+++ CGT14100-Final/html/response.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+"http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+	<title>Thank you for contacting us!</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">   	
+	<link href="CSS/master.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="header">
+		<div class="topnav">
+			<div class="dropdown">
+				<p class="dropbtn">More
+					<i class="fa fa-caret-down"></i>
+				</p>
+				<div class="dropdown-content">
+					<a href="about.html">About</a><br>
+					<a href="contact.html">Contact</a><br>
+					<a href="careers.html">Careers</a><br>
+					<a href="shop.html">Shop</a><br>
+				</div>
+			</div>
+			<a href="donate.html">Donate</a>
+			<a href="downloads.html">Downloads</a>
+			<a class="active" href="index.html">Home</a>
+		</div>
+	</div>
+	<p><h3>Hi, <?php echo $_POST["fname"]; ?>,<br></h3>
+	<p>Thank you for reaching out to us! We will get back to you shortly. While you wait, enjoy our downloadable frames!<br>
+	Thanks,<br>
+	<p>DMF Team</p>
+	<div class="footer">
+		<p><i>Purdue University, CGT 14100 Project Spring 2020<br>
+		This is just a joke, nothing serious or dangerous.</i><br>
+		<a href="index.html">downloadmasframes.com</a>   |   <a href="contact.html">Contact Us</a></p>
+		<p><a href="http://validator.w3.org/check?uri=referer">
+		<img src="http://www.w3.org/Icons/valid-html401" alt="Valid HTML 4.01 Strict" height="31" width="88"></a>
+		</p>
+	</div>
+</body>
+</html>
